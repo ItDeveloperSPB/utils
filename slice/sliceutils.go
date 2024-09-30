2b5b7aeca5ea9ed088d4d10ce27f928d1b9d4f58
nikolos <nikolos@nikoloss-MacBook-Pro.local>
2024-09-30 22:02:55 +0300

    add new method

--- slice/sliceutils.go
+++ slice/sliceutils.go
@@ -11,3 +11,24 @@ func RemoveAllInPlace(nums []int, value int) []int {
     }
     return nums[:newLength]
 }
+
+func RemoveFirst(nums []int, value int) []int {
+	for i, v := range nums {
+		if v == value {
+			// Удаляем элемент с индексом i
+			return append(nums[:i], nums[i+1:]...)
+		}
+	}
+	// Если элемент не найден, возвращаем оригинальный срез
+	return nums
+}
+
+func RemoveAllWithNewSlice(nums []int, value int) []int {
+	result := []int{}
+	for _, v := range nums {
+		if v != value {
+			result = append(result, v)
+		}
+	}
+	return result
+}
